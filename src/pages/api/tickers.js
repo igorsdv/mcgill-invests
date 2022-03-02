a2da15da4eb07c656cad9b2540aa9cee6a115521
Igor Sadikov <igorsadikov@gmail.com>
2022-03-02 15:55:10 -0500

    raw ticker sheet download

--- src/pages/api/tickers.js
+++ src/pages/api/tickers.js
@@ -1,5 +1,6 @@
 import { appendFile, writeFile } from 'fs/promises';
 import { getHoldingsByView, hydrateMetadata } from '../../lib/api.js';
+import { getHoldings } from '../../lib/holdings.js';
 import { chunk } from '../../lib/util.js';
 
 export default async function handler(req, res) {
@@ -7,35 +8,50 @@ export default async function handler(req, res) {
     return res.status(405).send();
   }
 
-  const outputPath = '/tmp/tickers.csv';
-  const holdings = (await getHoldingsByView('all')).sort(
-    (a, b) => b.marketValue - a.marketValue
-    // a.country.localeCompare(b.country) ||
-    // a.description1.localeCompare(b.description1)
+  const raw = !!req.query.raw;
+  const holdings = (
+    await (raw ? getHoldings() : getHoldingsByView('all'))
+  ).sort(
+    (a, b) =>
+      a.country.localeCompare(b.country) ||
+      a.description1.localeCompare(b.description1)
   );
 
-  const chunks = chunk(holdings, 20);
-
   res.status(200).send();
+
+  const chunks = chunk(holdings, 20);
+  const outputPath = raw ? '/tmp/raw-tickers.csv' : '/tmp/tickers.csv';
 
   await writeFile(outputPath, '');
 
   for (const [key, holdingsChunk] of Object.entries(chunks)) {
     console.log('Processing chunk', +key + 1, 'of', chunks.length);
 
-    await hydrateMetadata(holdingsChunk);
-    await appendFile(
-      outputPath,
-      holdingsChunk
-        .map(
-          ({
-            description1,
-            ticker,
-            country,
-            businessProfile: { name, symbol },
-          }) => [country, description1, ticker, symbol, name].join(',') + '\n'
-        )
-        .join('')
-    );
+    if (raw) {
+      await appendFile(
+        outputPath,
+        holdingsChunk
+          .map(
+            ({ description1, ticker, country, marketValue }) =>
+              [country, description1, ticker, marketValue].join(',') + '\n'
+          )
+          .join('')
+      );
+    } else {
+      await hydrateMetadata(holdingsChunk);
+      await appendFile(
+        outputPath,
+        holdingsChunk
+          .map(
+            ({
+              description1,
+              ticker,
+              country,
+              businessProfile: { name, symbol },
+            }) => [country, description1, ticker, symbol, name].join(',') + '\n'
+          )
+          .join('')
+      );
+    }
   }
 }
